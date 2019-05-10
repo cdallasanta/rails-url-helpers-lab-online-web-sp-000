206841b569d3d05aeb54c7742019b7b70f6cc32f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-10 16:31:57 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,7 @@ class StudentsController < ApplicationController
   def activate
     @student = set_student
     @student.active = !@student.active
+    @student.save
     redirect_to @student
   end
 
