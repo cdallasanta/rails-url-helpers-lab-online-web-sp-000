eeacbf4d166056fc7b33a27ac7ef0fe9f1a33c9f
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-10 16:24:55 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :students, only: [:index, :show, :edit]
+  resources :students, only: :edit, as: :activate
 
 end
