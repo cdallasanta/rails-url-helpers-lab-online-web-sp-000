73b7ccf0a50b5b826282b1c293438bcd989b3125
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-05-10 16:04:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 1) do
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.boolean  "status"
+    t.boolean  "status", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
